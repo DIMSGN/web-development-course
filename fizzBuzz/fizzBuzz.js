1dd56512e83aa95c00e05d35277133e6dc82827d
D. SIGANOS <147884676+DIMSGN@users.noreply.github.com>
2025-03-20 19:41:55 +0200

    fizzBuzz

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -1,13 +1,22 @@
 const rl = require('../readlineInterface');
 var output = [];
 var count = 1;
-function fizzBuzz() {
-output.push(count);
-count++;
-console.log(output);
-rl.question('Press Enter to run again or Ctrl+C to exit\n', () => {
-    fizzBuzz(); 
-});
 
+function fizzBuzz() {
+    if (count % 3 === 0 && count % 5 === 0) {
+        output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+        output.push("Fizz");
+    } else if (count % 5 === 0) {
+        output.push("Buzz");
+    } else {
+        output.push(count);
+    }
+    console.log(output);
+    count++;
+    rl.question('Press Enter to run again or Ctrl+C to exit\n', () => {
+        fizzBuzz();
+    });
 }
+
 module.exports = fizzBuzz;
